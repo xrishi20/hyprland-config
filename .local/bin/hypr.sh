c2b2f2c8c1c62378e1667475b50d9314f7aed2fa
xrishi20 <xrishi20@gmail.com>
2023-01-19 20:17:46 +0530

    Blur && Frozen

--- .local/bin/hypr.sh
+++ .local/bin/hypr.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # Session
-
 export LIBVA_DRIVER_NAME=nvidia
 export XDG_SESSION_TYPE=wayland
 export GBM_BACKEND=nvidia-drm
@@ -15,5 +14,7 @@ export WLR_NO_HARDWARE_CURSORS=1
 export MOZ_ENABLE_WAYLAND=1
 export _JAVA_AWT_WM_NONREPARENTING=1
 export XCURSOR_SIZE=24
+
+export QT_QPA_PLATFORMTHEME=qt5ct
 
 exec Hyprland
