a48a204c3efc4c2ef0acc42ba831f1badf5ce2d5
xrishi20 <xrishi20@gmail.com>
2023-02-16 13:44:55 +0530

    Neon Theme

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 # Packages
 
-hyprland-git waybar-hyprland-git git openssh kitty swaybg wofi dunst unzip fish starship nautilus udev-block-notify polkit-kde-agent seahorse gnome-keyring udisks2 brightnessctl bluez blueman pipewire wireplumber pipewire-pulse wl-clipboard r8168-dkms visual-studio-code-bin cloudflare-warp-bin catppuccin-gtk-theme-mocha papirus-icon-theme bibata-cursor-theme
+hyprland-git waybar-hyprland-git git openssh kitty swaybg wofi dunst unzip fish fish-done starship reflector nautilus udev-block-notify polkit-kde-agent seahorse gnome-keyring udisks2 brightnessctl bluez blueman pipewire wireplumber pipewire-pulse wl-clipboard r8168-dkms visual-studio-code-bin cloudflare-warp-bin catppuccin-gtk-theme-mocha papirus-icon-theme bibata-cursor-theme
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,72 +2,14 @@
 set fish_greeting
 set VIRTUAL_ENV_DISABLE_PROMPT "1"
 export BAT_THEME="Catppuccin-mocha"
-set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 
 # Set settings for https://github.com/franciscolourenco/done
 set -U __done_min_cmd_duration 10000
 set -U __done_notification_urgency_level low
-
-# Variables export 
-export QT_QPA_PLATFORM="wayland"
-#export ORG_OVERRIDE="catppuccin-rfc"
-
-# Add ~/.local/bin to PATH
-if test -d ~/.local/bin
-    if not contains -- ~/.local/bin $PATH
-        set -p PATH ~/.local/bin
-    end
-end
-
-# Add go path to PATH
-if test -d ~/go/bin/ 
-  if not contains -- ~/go/bin $PATH
-    set -p PATH ~/go/bin 
-    end 
-end
-
-# Enable touchpad gestures on firefox
-if [ $XDG_SESSION_TYPE = "wayland" ]
-  export MOZ_ENABLE_WAYLAND=1
-else
-  export MOZ_USE_XINPUT2=1
-end
-
-# Set GPG stuff
-export GPG_TTY=$(tty)
  
 # Starship prompt
 if status --is-interactive
  starship init fish | source
-end
-
-## Functions
-# Functions needed for !! and !$ https://github.com/oh-my-fish/plugin-bang-bang
-function __history_previous_command
-  switch (commandline -t)
-  case "!"
-    commandline -t $history[1]; commandline -f repaint
-  case "*"
-    commandline -i !
-  end
-end
-
-function __history_previous_command_arguments
-  switch (commandline -t)
-  case "!"
-    commandline -t ""
-    commandline -f history-token-search-backward
-  case "*"
-    commandline -i '$'
-  end
-end
-
-if [ "$fish_key_bindings" = fish_vi_key_bindings ];
-  bind -Minsert ! __history_previous_command
-  bind -Minsert '$' __history_previous_command_arguments
-else
-  bind ! __history_previous_command
-  bind '$' __history_previous_command_arguments
 end
 
 # Fish command history
@@ -80,23 +22,11 @@ function backup --argument filename
     cp $filename $filename.bak
 end
 
-# Replace some more things with better alternatives
-alias cat='bat --style header --style snip --style changes --style header'
-
-# Common use
-alias ..='cd ..'
-alias grep='grep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias egrep='egrep --color=auto'
-alias clear='clear && circles'
-
 # Pacman and reflector stuff
-alias listpkg="pacman -Qq | fzf --preview 'pacman -Qil {}' --layout=reverse --bind 'enter:execute(pacman -Qil {} | less)'"
-alias mirrorx='sudo reflector --age 6 --latest 200 --fastest 20 --threads 20 --sort rate --protocol https --verbose --save /etc/pacman.d/mirrorlist'
 alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
-alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
+alias listpkgs="pacman -Qq | fzf --preview 'pacman -Qil {}' --layout=reverse --bind 'enter:execute(pacman -Qil {} | less)'"
+alias mirror='sudo reflector --age 6 --latest 200 --fastest 20 --threads 20 --sort rate --protocol https --verbose --save /etc/pacman.d/mirrorlist'
 
-# Journalctl stuff
 # Get the error messages from journalctl
 alias jctl="journalctl -p 3 -xb"
 
